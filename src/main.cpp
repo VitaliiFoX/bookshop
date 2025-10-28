46da3d66cccd1c4f6d01e6f7e95eb1f48a064199
Vitalii Foiak <vitaliy.foyak@gmail.com>
2025-10-28 22:43:02 +0200

    initial structure

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include "auth/Auth.h"
+// #include "services/Services.h"
+
+void showServiceMenu() {
+    // books
+    // showBooks()
+    // // book
+    // showBook()
+    // // koshyk
+    // showKoshyk()
+}
+
+int main() {
+    while (true) {
+        showServiceMenu();
+    }
+}
