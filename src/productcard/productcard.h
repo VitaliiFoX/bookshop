4091867b3be6a8711b8962bc52a48347075acf97
zalevskyvova <zalevskyvova@gmail.com>
2025-11-25 23:16:46 +0200

    Add EditProductCard function declaration

--- src/productcard/productcard.h
+++ src/productcard/productcard.h
@@ -5,4 +5,5 @@ using namespace std;
 void ProductCardModule(vector<Book>& books);
 void CreateProductCard(vector<Book>& books);
 void CheckProductCard(const vector<Book>& books);
+void EditProductCard(vector<Book>& books);
 void RunApplication();
