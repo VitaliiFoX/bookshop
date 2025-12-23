965d7c65f54d9c7d38824837d80fa69a5c7e5375
DeltaTosh <yournameovertoken@gmail.com>
2025-12-23 10:05:21 +0200

    add removefromcart

--- src/Basket/basket.cpp
+++ src/Basket/basket.cpp
@@ -6,7 +6,7 @@ using namespace std;
 const int MAX_BOOKS = 100;
 
 // Функція для виведення списку книжок у кошику
-void showCart(vector<Basket> &basket) {
+void showCart(vector<Basket>& basket) {
     int count = basket.size();
     if (count == 0) {
         cout << "Кошик порожній.\n";
@@ -21,9 +21,9 @@ void showCart(vector<Basket> &basket) {
     for (int i = 0; i < count; i++) {
         double sum = basket[i].book.price * basket[i].quantity;
         total += sum;
-        cout << i + 1 << "\t" << basket[i].book.title << "\t\t" 
-             << basket[i].book.price << "\t" << basket[i].quantity 
-             << "\t\t" << sum << "\n";
+        cout << i + 1 << "\t" << basket[i].book.title << "\t\t"
+            << basket[i].book.price << "\t" << basket[i].quantity
+            << "\t\t" << sum << "\n";
     }
 
     cout << "-----------------------------------------------\n";
@@ -31,7 +31,7 @@ void showCart(vector<Basket> &basket) {
 }
 
 // Додати книгу до кошика
-void addToCart(vector<Basket> &basket, vector<Book> &books) {
+void addToCart(vector<Basket>& basket, vector<Book>& books) {
     int count = basket.size();
     if (count >= MAX_BOOKS) {
         cout << "Кошик переповнений!\n";
@@ -49,24 +49,46 @@ void addToCart(vector<Basket> &basket, vector<Book> &books) {
 
     for (auto book : books) {
         if (book.title.find(title) != string::npos) {
-            basket.push_back({book, qnty});
+            basket.push_back({ book, qnty });
         }
     }
 }
 
-void showBasket(vector<Basket> &basket, vector<Book> &books) {
-    int count = 0;
-    int choice;
+void removeFromCart(vector<Basket>& basket) {
+    int count = basket.size();
+    if (count == 0) {
+        cout << "Кошик порожній. Немає що видаляти.\n";
+        return;
+    }
+    showCart(vector<Basket> &basket);
 
-    do {
-        cout << "\n=== Меню книжкового магазину ===\n";
-        cout << "1. Додати книгу до кошика\n";
-        cout << "2. Переглянути кошик\n";
-        cout << "3. Вихід\n";
-        cout << "Ваш вибір: ";
-        cin >> choice;
+    cout << "Введіть номер книги для видалення: ";
+    int index;
+    cin >> index;
 
-        switch (choice) {
+    if (index < 1 || index > basket.size()) {
+        cout << "Невірний номер!\n";
+        return;
+
+        basket.erase(basket.begin() + index - 1);
+
+        cout << "Книгу успішно видалено з кошика.\n";
+    }
+
+    void showBasket(vector<Basket> &basket, vector<Book> &books) {
+        int count = 0;
+        int choice;
+
+        do {
+            cout << "\n=== Меню книжкового магазину ===\n";
+            cout << "1. Додати книгу до кошика\n";
+            cout << "2. Переглянути кошик\n";
+            cout << "3. Видалити книгу з кошика\n";
+            cout << "4. Вихід\n";
+            cout << "Ваш вибір: ";
+            cin >> choice;
+
+            switch (choice) {
             case 1:
                 addToCart(basket, books);
                 break;
@@ -74,10 +96,13 @@ void showBasket(vector<Basket> &basket, vector<Book> &books) {
                 showCart(basket);
                 break;
             case 3:
+                removeFromCart(basket);
+                break;
+            case 4:
                 cout << "Дякуємо за покупку!\n";
                 break;
             default:
                 cout << "Неправильний вибір!\n";
-        }
-    } while (choice != 3);
-}
+            }
+        } while (choice != 4);
+    }
